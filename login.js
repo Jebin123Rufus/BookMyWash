5ee76866a255f976ff1b40964d202a1829b72cf6
unknown <jebinrufuz@gmail.com>
2025-04-17 14:47:43 +0530

    Added regex to the login to ensure that the users are rit college students

--- login.js
+++ login.js
@@ -1,38 +1,38 @@
 console.log("Script loaded");
 
-document.getElementById('loginForm').addEventListener('submit', async function (e) {
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
     e.preventDefault();
     console.log("Form submitted");
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    console.log("Sending data:", { name, email });
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const regex = /^[a-z]+\.[0-9]{6}@[a-z]{3,5}\.ritchennai\.edu\.in$/;
 
-    try {
-        const res = await fetch('http://localhost:5000/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name, email })
+    if (regex.test(email)) {
+      try {
+        const res = await fetch("http://localhost:5000/api/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name, email }),
         });
 
         const data = await res.json();
         console.log("Server response:", data);
 
         if (res.ok) {
-            alert(data.message);
-            // Save login state
-            localStorage.setItem('isLoggedIn', 'true');
-            localStorage.setItem('username', name); // optional
-            window.location.href = 'index.html';
+          alert(data.message);
+          // Save login state
+          localStorage.setItem("isLoggedIn", "true");
+          localStorage.setItem("username", name); // optional
+          window.location.href = "index.html";
         } else {
-            alert('Login failed: ' + data.message);
+          alert("Login failed: " + data.message);
         }
-
-    } catch (err) {
+      } catch (err) {
         console.error("Request failed:", err);
         alert("Error submitting form");
+      }
+    } else {
+      alert("Invalid email! Enter a valid college mail-id to Login");
     }
-});
-
-
-
+  });
